7e5ff6055ef8723245450ce76535c5945d1be1ab
Adarsh KM <65649644+ADARSHKM2001@users.noreply.github.com>
2020-07-21 13:11:41 +0530

    Add files via upload

---
+++ strrev using pointers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    char str[50];
+    char rev[50];
+    char *sptr = str; 
+    char *rptr = rev;
+    int i = -1;
+    printf("\n\nEnter a string: ");
+    scanf("%s", str);
+    while(*sptr)
+    {
+        sptr++;
+        i++; 
+    }
+    while(i >= 0)
+    {
+        sptr--; 
+        *rptr = *sptr; 
+        rptr++;
+        i--;    
+    }
+    *rptr = '\0'; 
+    rptr = rev;
+    while(*rptr)
+    {
+        *sptr = *rptr;
+        sptr++;
+        rptr++;
+    }
+    printf("\n\nReverse of the string is: %s ", str);
+    return 0;
+}
